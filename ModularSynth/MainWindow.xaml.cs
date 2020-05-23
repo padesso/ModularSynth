2f61a988b82aed9da185e5c740b1b6ccd8c3c5c0
Patrick Adesso <padesso@deximaging.com>
2020-05-22 22:05:07 -0400

    Add project files.

---
+++ ModularSynth/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ModularSynth.WaveProviders;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModularSynth
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WaveOut waveOut;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartStopSineWave();
+        }
+
+        private void StartStopSineWave()
+        {
+            if (waveOut == null)
+            {
+                var sineWaveProvider = new SineWaveProvider32();
+                sineWaveProvider.SetWaveFormat(16000, 1); // 16kHz mono
+                sineWaveProvider.Frequency = 1000;
+                sineWaveProvider.Amplitude = 0.25f;
+                waveOut = new WaveOut();
+                waveOut.Init(sineWaveProvider);
+                waveOut.Play();
+            }
+            else
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+    }
+}
