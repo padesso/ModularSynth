848ec91b26fb101743038b9a5f7d830af95ac23a
Patrick Adesso <padesso@deximaging.com>
2020-05-22 23:16:46 -0400

    Bound sliders to freq and amp of sine.

--- ModularSynth/ViewModels/MainViewModel.cs
+++ ModularSynth/ViewModels/MainViewModel.cs
@@ -11,10 +11,23 @@ namespace ModularSynth.ViewModels
     public class MainViewModel : ViewModelBase
     {
         private WaveOut waveOut;
+        private SineWaveProvider32 sineWaveProvider;
 
         public MainViewModel()
         {
             StartStopSinCommand = new RelayCommand(StartStopSineWave);
+
+            sineWaveProvider = new SineWaveProvider32(); //TODO: pass initial freq and amplitude to ctor?  
+            sineWaveProvider.SetWaveFormat(16000, 1); // 16kHz mono
+            waveOut = new WaveOut();
+            waveOut.Init(sineWaveProvider);
+
+            //Start values
+            Frequency = 1000;
+            Amplitude = 0.25f;
+
+            sineWaveProvider.Frequency = Frequency;
+            sineWaveProvider.Amplitude = Amplitude;
         }
 
         public RelayCommand StartStopSinCommand
@@ -23,23 +36,37 @@ namespace ModularSynth.ViewModels
             private set;
         }
 
+        private float frequency;
+        public float Frequency
+        {
+            get => frequency;
+            set
+            {
+                Set(ref frequency, value);
+                sineWaveProvider.Frequency = Frequency;
+            }
+        }
+
+        private float amplitude;
+        public float Amplitude
+        {
+            get => amplitude;
+            set
+            {
+                Set(ref amplitude, value);
+                sineWaveProvider.Amplitude = Amplitude;
+            }
+        }
+
         private void StartStopSineWave()
         {
-            if (waveOut == null)
-            {
-                var sineWaveProvider = new SineWaveProvider32();
-                sineWaveProvider.SetWaveFormat(16000, 1); // 16kHz mono
-                sineWaveProvider.Frequency = 1000;
-                sineWaveProvider.Amplitude = 0.25f;
-                waveOut = new WaveOut();
-                waveOut.Init(sineWaveProvider);
-                waveOut.Play();
+            if (waveOut.PlaybackState == PlaybackState.Playing)
+            {     
+                waveOut.Pause();
             }
             else
             {
-                waveOut.Stop();
-                waveOut.Dispose();
-                waveOut = null;
+                waveOut.Play();
             }
         }
     }
