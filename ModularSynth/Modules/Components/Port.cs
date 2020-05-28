a7b87dd177d1973469da31795f0fae107d5907ce
Patrick Adesso <padesso@deximaging.com>
2020-05-27 20:12:39 -0400

    Starting to feature branch.

--- ModularSynth/Modules/Components/Port.cs
+++ ModularSynth/Modules/Components/Port.cs
@@ -6,6 +6,24 @@ namespace ModularSynth.Modules.Components
 {
     public class Port
     {
-        public string Name;
+        public enum PortType
+        {
+            ControlVoltagePort,
+            AudioPort
+        }
+
+        public string Name { get; set; }
+        public PortType Type { get; set; }
+        
+        public Port(string name, PortType portType)
+        {
+
+        }
+
+        public Port()
+        {
+            Name = "Default";
+            Type = PortType.ControlVoltagePort;
+        }
     }
 }
---
+++ ModularSynth/Modules/Gates/ButtonModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModularSynth.Modules.Gates
+{
+    public class ButtonModule : ModuleBase
+    {
+        public ButtonModule()
+        {
+
+        }
+    }
+}
